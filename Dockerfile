e78091ca96539a67591492f978a25ed0909ac575
JianmingXia <1179801728@qq.com>
2020-12-21 12:16:56 +0800

    feat: puppeteer-core and google-chrome

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,22 @@
-FROM node:8.11
+FROM centos:7
+
+RUN \
+  yum -y update \
+  && yum -y install wget \
+  && wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm \
+  && yum -y install ./google-chrome-stable_current_*.rpm
+
+# FROM node:14.4.0
+COPY --from=node:14.4.0 /usr/local/include/node /usr/local/include/node
+COPY --from=node:14.4.0 /usr/local/bin/node /usr/local/bin/node
+COPY --from=node:14.4.0 /usr/local/lib/node_modules /usr/local/lib/node_modules
+COPY --from=node:14.4.0 /usr/local/share/doc/node /usr/local/share/doc/node
+
+RUN \
+  ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm \
+  && ln -s /usr/local/lib/node_modules/npm/bin/npx-cli.js /usr/local/bin/npx \
+  && node --version \
+  && npm --version
 
 # Create app directory
 WORKDIR /usr/src/app
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require("express");
+const Koa = require("koa");
+const puppeteer = require("puppeteer-core");
+// const command = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+const command = 'google-chrome'
+
+const app = new Koa();
+
+app.use(async ctx => {
+  console.log(`${new Date()}receiving request....`);
+  const browser = await puppeteer.launch({executablePath: command, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto("https://segmentfault.com/", { waitUntil: "networkidle2" });
+  const pdf = await page.pdf({ format: "A4" });
+
+  await browser.close();
+
+  ctx.set('Content-Type', 'application/pdf')
+  ctx.set('Content-Disposition', 'attachment; filename="test.pdf";')
+  ctx.body = pdf;
+});
 
 // Constants
 const PORT = 3000;
 const HOST = "0.0.0.0";
-
-// App
-const app = express();
-app.get("/", (req, res) => {
-  console.log(`${new Date()}receiving request....`);
-  res.send("Hello Docker!\n");
-});
-
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
